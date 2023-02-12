2240cce10cec2deb33ac1018d8d9653dbe13a719
marcelosantana <marcelosantana@alu.ufc.br>
2023-02-12 10:23:26 -0300

    [Daily-Diet] - First Commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { HomeContainer } from "./styles";
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Text>Hello World</Text>
+    </HomeContainer>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const HomeContainer = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
